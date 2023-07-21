352bc7b3a65cc8625b26aff1ff09e172c1338f8e
GildedJson <sms.tabaian@gmail.com>
2023-07-21 19:26:45 +0330

    Write code for Rectangle

--- src/main/Rectangle.java
+++ src/main/Rectangle.java
@@ -1,5 +1,31 @@
 package main;
 
 public class Rectangle {
+    private int width;
+    private int height;
 
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int calculateArea() {
+        return width * height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }
