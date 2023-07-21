0149736ede96687331ac8d64e40fa790702c7347
GildedJson <sms.tabaian@gmail.com>
2023-07-21 19:25:22 +0330

    Write a test

---
+++ src/main/RectangleTest.java
@@ -0,0 +1,14 @@
+package main;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RectangleTest {
+
+    @Test
+    void calculateArea1() {
+        Rectangle rex = new Rectangle(4, 6);
+        assertEquals(24, rex.calculateArea());
+    }
+}
